db86e909da5d3c2c9698b9b7dfe871eb3edd1a51
gPioro <gpioro2001@gmail.com>
2023-04-25 12:15:13 +0200

    Projekt z programu CubeMX

---
+++ SR_projekt_Cube/CubeMX/Debug/Core/Src/driver_aht20.d
@@ -0,0 +1,3 @@
+Core/Src/driver_aht20.o: ../Core/Src/driver_aht20.c \
+ ../Core/Inc/driver_aht20.h
+../Core/Inc/driver_aht20.h:
---
+++ SR_projekt_Cube/CubeMX/Debug/Core/Src/driver_aht20_read_test.d
@@ -0,0 +1,6 @@
+Core/Src/driver_aht20_read_test.o: ../Core/Src/driver_aht20_read_test.c \
+ ../Core/Inc/driver_aht20_read_test.h \
+ ../Core/Inc/driver_aht20_interface.h ../Core/Inc/driver_aht20.h
+../Core/Inc/driver_aht20_read_test.h:
+../Core/Inc/driver_aht20_interface.h:
+../Core/Inc/driver_aht20.h:
